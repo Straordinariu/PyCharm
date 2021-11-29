c7033120abc4a0bb825e6ce3a5c28606a63c9d0c
Straordinariu <sasha4983@mail.ru>
2021-11-29 05:33:54 +0500

    9

--- filter.py
+++ filter.py
@@ -2,8 +2,24 @@
 from PIL import Image
 import numpy as np
 
+
 def median_gray(arr, step):
+    """
+        Находит самое близкое к среднему значению массива arr число, кратное шагу step.
+        :param arr: array
+        :param step: int
+        :return: int
+        >>> median_gray([0, 50, 100], 50)
+        50
+        >>> median_gray([0, 1, 2], 50)
+        0
+        >>> median_gray([0, 50, 250], 50)
+        100
+        >>> median_gray([40, 28, 61], 1)
+        43
+    """
     return int(np.mean(arr) // step) * step
+
 
 def gray_pixelation(image, size, gradations):
     step = int(255 // (gradations - 1))
@@ -13,8 +29,6 @@ def gray_pixelation(image, size, gradations):
             arr[row:row + size, column:column + size] = \
                 median_gray(arr[row:row + size, column:column + size], step)
     return Image.fromarray(arr)
-
-
 if __name__ == "__main__":
     # gray_pixelation(Image.open(sys.argv[1]), 10, 5).save(sys.argv[2])
     gray_pixelation(
